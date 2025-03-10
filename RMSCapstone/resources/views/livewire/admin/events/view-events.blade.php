b0cae654e7c747461cafdc868f5786c6605bc2aa
krstinkyl <israelkristinekyle@gmail.com>
2025-03-11 01:58:03 +0800

    type sort
    added all on type sort

--- RMSCapstone/resources/views/livewire/admin/events/view-events.blade.php
+++ RMSCapstone/resources/views/livewire/admin/events/view-events.blade.php
@@ -36,6 +36,7 @@
                             <label class="w-40 text-sm font-medium text-gray-900">Event Status :</label>
                             <select wire:model.live="eventStatus"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
+                                <option value="">All</option>
                                 <option value="confirmed">Confirmed</option>
                                 <option value="on-going">On-going</option>
                                 <option value="cancelled">Cancelled</option>
