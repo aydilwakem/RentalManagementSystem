06d5aeda4b9e0e368e3d491c3be45a6883bf6ab3
Barbie Jalandoni <jalandonibarbie940@gmail.com>
2025-04-01 01:23:02 +0800

    Updated no access

--- RMSCapstone/resources/views/livewire/no-access.blade.php
+++ RMSCapstone/resources/views/livewire/no-access.blade.php
@@ -1,6 +1,9 @@
-<div class="min-h-[550px] container mx-auto p-6 bg-white rounded-lg">
+<div class="min-h-[550px] container mx-auto p-6 bg-white rounded-lg flex flex-col items-center">
+    <img src="{{ asset('storage/carl.jpg') }}" alt="No Roles Assigned" class="w-40 h-40 mb-4">
     <div class="text-center py-10">
-        <p class="text-gray-500 text-lg font-semibold">You don't have any roles assigned yet.<br> This
-            is why you can't access any pages. Please contact the admin.</p>
+        <p class="text-gray-500 text-lg font-semibold">
+            You don't have any roles assigned yet.<br>
+            This is why you can't access any pages. Please contact the admin.
+        </p>
     </div>
 </div>
