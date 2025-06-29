93279aadd2c7df220b81bb812660ff8008ffa49e
krstinkyl <israelkristinekyle@gmail.com>
2025-06-29 14:27:37 +0800

    update profile information test

--- RMSCapstone/tests/Feature/ProfileInformationTest.php
+++ RMSCapstone/tests/Feature/ProfileInformationTest.php
@@ -25,13 +25,31 @@ class ProfileInformationTest extends TestCase
 
     public function test_profile_information_can_be_updated(): void
     {
-        $this->actingAs($user = User::factory()->create());
+       
+        $user = User::factory()->create([
+            'name' => 'Original Name',
+            'last_name' => 'Original',
+            'middle_name' => 'Orginal',
+            'suffix' => 'Orginal',
+            'email' => 'original@example.com',
+            'email_verified_at' => now(),
+        ]);
+
+        $this->actingAs($user);
 
         Livewire::test(UpdateProfileInformationForm::class)
-            ->set('state', ['name' => 'Test Name', 'email' => 'test@example.com'])
-            ->call('updateProfileInformation');
+            ->set('state.name', 'Test Name') 
+            ->set('state.midlle_name', 'Test Name') 
+            ->set('state.last_name', 'Test Name') 
+            ->set('state.suffic', 'Test Name') 
+            ->set('state.email', 'test@example.com')
+            ->call('updateProfileInformation')
+            ->assertHasNoErrors();
 
-        $this->assertEquals('Test Name', $user->fresh()->name);
-        $this->assertEquals('test@example.com', $user->fresh()->email);
+        $user->refresh();
+
+        $this->assertEquals('Test Name', $user->name);
+        $this->assertEquals('test@example.com', $user->email);
+        $this->assertNull($user->email_verified_at); // email change resets verification
     }
 }
