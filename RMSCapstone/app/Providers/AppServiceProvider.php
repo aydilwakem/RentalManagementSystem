5f53369cead58343299beb980fb8e440ae0a88b2
krstinkyl <israelkristinekyle@gmail.com>
2025-09-05 16:09:21 +0800

    Superadmin gate

--- RMSCapstone/app/Providers/AppServiceProvider.php
+++ RMSCapstone/app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Relations\Relation;
 use App\Models\Property;
 use App\Models\Room;
 use App\Models\EventHall;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,12 +22,24 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
+
+    /**
+     * Superadmin Gate for all permissions
+     */
     public function boot()
     {
+         Gate::before(function ($user, $ability) {
+        return $user->hasRole('Super Admin') ? true : null;
+    });
+
         Relation::morphMap([
             1 => \App\Models\Property::class,
             2 => \App\Models\Room::class,
             3 => \App\Models\EventHall::class,
         ]);
+
+
+
+
     }
 }
